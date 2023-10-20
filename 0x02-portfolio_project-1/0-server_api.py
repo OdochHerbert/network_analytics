fae4615cc918147b87e58f92cb9d37dcba989312
OdochHerbert <odoherb@gmail.com>
2023-10-20 01:17:03 +0000

    biul

---
+++ 0x02-portfolio_project-1/0-server_api.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+# Function to execute shell commands
+def run_command(command):
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+    if result.returncode == 0:
+        return result.stdout.strip()
+    else:
+        return result.stderr
+
+# Example route to get network interfaces
+@app.route('/network/interfaces', methods=['GET'])
+def get_network_interfaces():
+    command = "ip link show"
+    network_interfaces = run_command(command)
+    return jsonify({'network_interfaces': network_interfaces})
+
+# Example route to get IP addresses
+@app.route('/network/ip_addresses', methods=['GET'])
+def get_ip_addresses():
+    command = "ip addr show"
+    ip_addresses = run_command(command)
+    return jsonify({'ip_addresses': ip_addresses})
+
+# Example route to get routing tables
+@app.route('/network/routing_tables', methods=['GET'])
+def get_routing_tables():
+    command = "ip route show"
+    routing_tables = run_command(command)
+    return jsonify({'routing_tables': routing_tables})
+
+# Example route to get network statistics
+@app.route('/network/statistics', methods=['GET'])
+def get_network_statistics():
+    command = "netstat -s"
+    network_statistics = run_command(command)
+    return jsonify({'network_statistics': network_statistics})
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=5007)  # Set the port to 5007
